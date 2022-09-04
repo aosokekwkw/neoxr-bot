92ee590595c1f24666abb21e62fb090a96dd4bc2
aosokekwkw <112259923+aosokekwkw@users.noreply.github.com>
2022-09-04 09:10:55 +0800

    Update and rename system/config.js to sistem/config.js

--- sistem/config.js
+++ sistem/config.js
@@ -1,7 +1,7 @@
 // Owner number
-global.owner = '6282141544557'
+global.owner = '6287837703726'
 // Owner name
-global.owner_name = 'Customer Service'
+global.owner_name = 'FauziGntng'
 // Maximum upload file size limit (Default : 250 MB)
 global.max_upload = 250
 // Delay for spamming protection (Default : 3 seconds)
@@ -15,7 +15,7 @@ global.evaluate_chars = ['=>', '~>', '<', '>', '$']
 // Country code that will be automatically blocked by the system, when sending messages in private chat
 global.blocks = ['91', '92', '212']
 // Put target jid to forward friends story
-global.forwards = '6282141544557@c.us'
+global.forwards = '6287837703726@c.us'
 // Get neoxr apikey by registering at https://api.neoxr.my.id
 global.Api = new (require('./neoxrApi'))('5VC9rvNx')
 // Get bid and key configuration for autoreply chat ai feature by registering at https://brainshop.ai
