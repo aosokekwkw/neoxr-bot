a0867ed000638ea7acd57daa5e5e83339b0e02ba
aosokekwkw <112259923+aosokekwkw@users.noreply.github.com>
2022-09-04 09:35:22 +0800

    Add files via upload

--- system/config.js
+++ system/config.js
@@ -1,7 +1,7 @@
 // Owner number
-global.owner = '6282141544557'
+global.owner = '6287837703726'
 // Owner name
-global.owner_name = 'Customer Service'
+global.owner_name = 'Fauzigntng'
 // Maximum upload file size limit (Default : 250 MB)
 global.max_upload = 250
 // Delay for spamming protection (Default : 3 seconds)
@@ -15,28 +15,28 @@ global.evaluate_chars = ['=>', '~>', '<', '>', '$']
 // Country code that will be automatically blocked by the system, when sending messages in private chat
 global.blocks = ['91', '92', '212']
 // Put target jid to forward friends story
-global.forwards = '6282141544557@c.us'
+global.forwards = '6287837703726@c.us'
 // Get neoxr apikey by registering at https://api.neoxr.my.id
 global.Api = new (require('./neoxrApi'))('5VC9rvNx')
 // Get bid and key configuration for autoreply chat ai feature by registering at https://brainshop.ai
 global.chatai_bid = '164728'
 global.chatai_key = 'MKPsfkgXLZPGrWoH'
 // Footer text
-global.footer = 'ꜱɪᴍᴘʟᴇ ᴡʜᴀᴛꜱᴀᴘᴘ ʙᴏᴛ ᴍᴀᴅᴇ ʙʏ ɴᴇᴏxʀ ッ'
+global.footer = 'ꜱɪᴍᴘʟᴇ ᴡʜᴀᴛꜱᴀᴘᴘ ʙᴏᴛ ᴍᴀᴅᴇ ʙʏ FauziBotz-Md ッ'
 // Global status
 global.status = Object.freeze({
-   wait: Func.texted('bold', 'Processed . . .'),
-   invalid: Func.texted('bold', 'URL is Invalid!'),
-   wrong: Func.texted('bold', 'Wrong format!'),
-   getdata: Func.texted('bold', 'Scraping metadata . . .'),
-   fail: Func.texted('bold', 'Can\'t get metadata!'),
-   error: Func.texted('bold', 'Error occurred!'),
-   errorF: Func.texted('bold', 'Sorry this feature is in error.'),
-   premium: Func.texted('bold', 'This feature only for premium user.'),
-   owner: Func.texted('bold', 'This command only for owner.'),
-   god: Func.texted('bold', 'This command only for Master'),
-   group: Func.texted('bold', 'This command will only work in groups.'),
-   botAdmin: Func.texted('bold', 'This command will work when I become an admin.'),
-   admin: Func.texted('bold', 'This command only for group admin.'),
-   private: Func.texted('bold', 'Use this command in private chat.')
+   wait: Func.texted('bold', 'Diproses . . .'),
+   invalid: Func.texted('bold', 'URL Tidak Valid!'),
+   wrong: Func.texted('bold', 'Format yang salah!'),
+   getdata: Func.texted('bold', 'Menggores metadata . . .'),
+   fail: Func.texted('bold', 'Tidak bisa mendapatkan metadata!'),
+   error: Func.texted('bold', 'Terjadi kesalahan!'),
+   errorF: Func.texted('bold', 'Maaf fitur ini sedang error.'),
+   premium: Func.texted('bold', 'Fitur ini hanya untuk pengguna premium.'),
+   owner: Func.texted('bold', 'Perintah ini hanya untuk pemilik.'),
+   god: Func.texted('bold', 'Perintah ini hanya untuk Guru'),
+   group: Func.texted('bold', 'Perintah ini hanya akan bekerja dalam kelompok.'),
+   botAdmin: Func.texted('bold', 'Perintah ini akan berfungsi ketika saya menjadi admin.'),
+   admin: Func.texted('bold', 'Perintah ini hanya untuk admin grup.'),
+   private: Func.texted('bold', 'Gunakan perintah ini di cha pribadit.')
 })
